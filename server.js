324779858f1450c5bc2cbccdaa43bacefb37376f
smchughinfo <seanmchugh.info@gmail.com>
2025-07-06 06:11:44 -0400

    update default port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const activityService = require('./services/activityService');
 const trackedActivityService = require('./services/trackedActivityService');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Parse JSON bodies
 app.use(express.json());
